faaa5e6920050eafad2ec042c8f7215ea9866964
Max <maxj@smax-n-cheese.mit.edu>
2014-08-22 20:06:57 -0400

    Replicated memorydb functionality

--- BaseStation/db.py
+++ BaseStation/db.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python
+import sqlite3 as sq
+import pickle
 
 class Database:
 	def __init__(self):
-		pass
+		self.conn = sq.connect('guns.db')
+		self.c = self.conn.cursor()
+	def new_gun_id(self):
+		m = self.c.execute('SELECT MAX(gun_id) FROM guns').fetchone()
+		return max(0x81, (m[0] or 0) + 1)
+	def confirm_id(self, val):
+		self.c.execute("INSERT INTO guns VALUES({0}, '', '', '')".format(int(val)))
+		self.conn.commit()
+	def add_name(self, gun_id, name):
+		self.c.execute("UPDATE guns SET athena=? WHERE gun_id=?", (name, gun_id,))
+	def get_name_from_gun(self, gun_id):
+		return self.c.execute("SELECT athena FROM guns WHERE gun_id=?", (gun_id,)).fetchone()[0]
+	def dump_db(self):
+		return self.c.execute("SELECT * from guns")
+	def close(self):
+		self.conn.close()
 
 class RAMDatabase:
 	def __init__(self):
@@ -23,3 +40,13 @@ class RAMDatabase:
 			return self.names[gun_id]
 		else:
 			return None
+if __name__ == '__main__':
+	db = Database()
+	n = db.new_gun_id()
+	db.confirm_id(n)
+	db.add_name(n, "maxj")
+	print db.get_name_from_gun(n)
+	for row in db.dump_db():
+		print row
+	db.close()
+	
